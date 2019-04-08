c69ebe9bbdeca07b483e7c5adc3a362546c03808
Brett Carroll <brett.carroll@fiscal.treasury.gov>
2019-04-08 16:14:20 -0400

    add request_wants_json()

--- lib/utils.py
+++ lib/utils.py
@@ -1,11 +1,18 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
 
 # Import required .NET Assemblies
 import clr
 clr.AddReference('System.Security.Principal')
 from System.Security.Principal import WindowsIdentity, WindowsPrincipal, WindowsBuiltInRole
 
+from flask import request
+
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
 
 def isAdmin():
     """ Determine if the user executing this script has Admin rights """
