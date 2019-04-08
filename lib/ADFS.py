2c193b9ae059ba9277a4b75e0789526b58d31608
Brett Carroll <brett.carroll@fiscal.treasury.gov>
2019-04-08 16:16:06 -0400

    use psutil instead of PowerShell call

--- lib/ADFS.py
+++ lib/ADFS.py
@@ -3,11 +3,13 @@ from lib.PowerShell import psexec
 import clr
 clr.AddReference('System')
 from System import ArgumentOutOfRangeException
+from psutil import win_service_get
 
 def ServiceStatus(name="adfssrv"):
     serviceInfo = "Not Found"
     try:
-        serviceInfo = psexec("Get-Service -Name %s")[0] % name
-    except ArgumentOutOfRangeException as e:
-        logging.error("Service not found on system (%s)" % name)
+        serviceInfo = win_service_get(name)
+        serviceInfo = serviceInfo.as_dict()
+    except Exception as e:
+        logging.error("Service not found (%s)" % str(name))
     return (serviceInfo)
