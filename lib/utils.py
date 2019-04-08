4390b43e57a62f6ff0dad7de0deb5f17056d4df6
Brett Carroll <brettwcarroll@hotmail.com>
2019-04-07 20:45:10 -0400

    Added lib.utils.isAdmin()

--- lib/utils.py
+++ lib/utils.py
@@ -1,2 +1,20 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
+
+# Import required .NET Assemblies
+import clr
+clr.AddReference('System.Security.Principal')
+from System.Security.Principal import WindowsIdentity, WindowsPrincipal, WindowsBuiltInRole
+
+
+def isAdmin():
+    """ Determine if the user executing this script has Admin rights """
+    wi = WindowsIdentity.GetCurrent()
+    wp = WindowsPrincipal(wi)
+    if wp.IsInRole(WindowsBuiltInRole.Administrator):
+        return True
+    else:
+        return False
+
+print( isAdmin() )
+
