e9b978638b0faedd49b025c52cbee5b024fd8145
Brett Carroll <brett.carroll@fiscal.treasury.gov>
2019-04-08 16:14:44 -0400

    TotalMemory() always returns bytes

--- lib/WindowsOS.py
+++ lib/WindowsOS.py
@@ -3,11 +3,16 @@ from lib.PowerShell import psexec
 from pprint import pprint
 
 def WindowsVersion():
+    """
+    :return: Windows Operating System Version
+    """
     return( psexec("[environment]::OSVersion.Version")[0] )
 
-def TotalMemory(format='bytes'):
+def TotalMemory():
+    """
+    :return:
+    Total memory in bytes
+    """
     totalMemory = psexec( "(Get-CimInstance -ClassName 'Cim_PhysicalMemory' | Measure-Object -Property Capacity -Sum).Sum")[0]
-    if format == 'bytes':
-        return("%s" % totalMemory)
-    elif format == 'kbytes':
-        return("%s KB" % totalMemory/1024)
+    return("%s" % totalMemory)
+    
