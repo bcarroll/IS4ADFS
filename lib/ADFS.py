7d7488098d73906022806d04fb944559c85be36c
Brett Carroll <brett.carroll@fiscal.treasury.gov>
2019-04-08 08:51:39 -0400

    added ServiceStatus() method

---
+++ lib/ADFS.py
@@ -0,0 +1,12 @@
+import logging
+from lib.PowerShell import psexec
+import clr
+clr.AddReference('System')
+from System import ArgumentOutOfRangeException
+
+def ServiceStatus(name="adfssrv"):
+    serviceInfo = None
+    try:
+        serviceInfo = psexec("Get-Service -Name %s")[0] % name
+    except ArgumentOutOfRangeException as e:
+        logging.error("Service not found on system (%s)" % name)
