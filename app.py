1bff23fb090a651a19d2d7b573ea04e27459e8fd
Brett Carroll <brettwcarroll@hotmail.com>
2019-04-07 22:59:51 -0400

    implement isAdmin()

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from sys import path as sys_path
+import logging
+
+from sys import path as sys_path, exit as sys_exit
 sys_path.append(".")
 
 from flask import Flask, jsonify
 
+from lib.utils import isAdmin
 from lib.WindowsOS import WindowsVersion
+
 from views import config
 
 VERSION = '0.0.1'
@@ -12,14 +16,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({
-            "name": "ADFS Integration Service", 
+            "name": "ADFS Integration Service",
             "version": "%s" % (VERSION)
         })
 
+##############################################################################
+# /config/
 @app.route('/config/env')
 def env():
-    #return jsonify({"OSVersion": "%s" % WindowsVersion()})
     return(config.env())
 
+##############################################################################
+# Main entry point
 if __name__ == '__main__':
+    if not isAdmin():
+        errorMessage = "Privelege error: Administrative priveleges required to manage ADFS."
+        logging.error(errorMessage)
+        raise WindowsError(errorMessage)
+        sys_exit(1)
     app.run(debug=True)
